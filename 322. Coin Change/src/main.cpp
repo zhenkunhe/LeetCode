0c9160eb04233292231d404df430f001d87193ff
TrashMan <zhenkunhe@gmail.com>
2018-12-28 18:17:45 +0800

    [Add] 322.

---
+++ 322. Coin Change/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+class Solution
+{
+public:
+	int coinChange(vector<int>& coins, int amount)
+	{
+		sort(coins.rbegin(), coins.rend());
+		int min = INT_MAX;
+		coinChange(coins, amount, 0, 0, min);
+		return min == INT_MAX ? -1 : min;
+	}
+private:
+	void coinChange(vector<int>& coins, int amount, int count, int index, int& res)
+	{
+		if (amount == 0)
+		{
+			if (count < res) res = count;
+			return;
+		}
+		if (index == coins.size()) return;
+
+		for (int k = amount / coins[index]; k >= 0 && count + k < res; k--)
+			coinChange(coins, amount - k * coins[index], count + k, index + 1, res);
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> coins { 1, 2, 5 };
+	int amount = 11;
+	s.coinChange(coins, amount);
+	return 0;
+}
